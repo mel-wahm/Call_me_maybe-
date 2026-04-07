9715399abc3c10c382e5b92127d8deffe3ec8649
mel-wahm <muhammadelqassim03@gmail.com>
2026-04-07 08:01:27 +0100

    Refactor __main__.py for function management and extraction
    Refactor main script to load function definitions and manage function calls. Added functionality to extract parameters and build answers based on user requests.

--- src/__main__.py
+++ src/__main__.py
@@ -1,62 +1,102 @@
 from llm_sdk import Small_LLM_Model
-import json
+from os import system
+from json import load
+
 model = Small_LLM_Model()
+encode = model.encode
+decode = model.decode
+log = model.get_logits_from_input_ids
 
-def get_sceleton():
-    definitions = open('data/input/functions_definition.json', 'r')
-    defs = json.load(definitions)
-    
-    functions = {}
-    for function in defs:
-        functions[function['name']] = (function['parameters'])
-    return functions
 
-sceleton = get_sceleton()
+with open(model.get_path_to_vocab_file(), 'r') as f:
+    tokens = load(f)
 
-parameter  = list(sceleton['fn_add_numbers'].keys())[0]
+with open('data/data/input/functions_definition.json') as f:
+    functions = load(f)
 
-type_of_parameter = sceleton['fn_add_numbers'][parameter]['type']
+system('clear')
+desc = {}
+function_names = []
 
-def json_prefix(prompt, function_name, sceleton):
-    para = '"' + list(sceleton[function_name].keys())[0] + '"' + ':' 
-    return f"""
-    {{
-    "prompt": "{prompt}",
-    "name": "{function_name}",
-    "parameters": {{{para}
+for fun in functions:
+    desc[fun['name']] = fun['description']
+    function_names.append(fun['name'])
+
+
+def get_function_names(user_request):
+
+    prompt = f"""
+
+    <|im_start|>system
+    You are a function calling assistant. Given a user request, select the correct function from the list below.
+
+    AVAILABLE FUNCTIONS
+    {desc}
+    <|im_end|>
+    <|im_start|>user
+    {user_request}
+    <|im_end|>
+    <|im_start|>assistant
+    fn_
     """
+    newly_generated = ""
+    while True:
+        logits = model.get_logits_from_input_ids((encode(prompt + newly_generated)[0]).tolist())
+        for k, v in tokens.items():
+            if all(not f.startswith(newly_generated + k) for f in function_names):
+                logits[v] = float("-inf")
 
-path_to_tokens = model.get_path_to_vocab_file()
-with open(path_to_tokens, 'r') as f:
-    tokens = json.load(f)
+        for i in range(len(tokens), len(logits)):
+            logits[i] = float('-inf')
 
-def strip_space(token):
-    resulted_token = ""
-    for char in token:
-        if char != 'Ġ':
-            resulted_token += char
-    return resulted_token
+        valid = [i for i in range(len(logits)) if logits[i]!= float('-inf')]
+        valid = sorted(valid, key=lambda x: logits[x], reverse=True)
+        newly_generated += decode([logits.index(max(logits))])
 
-def one_num(token):
-    digits = "0123456789"
-    for char in token:
-        if char in digits:
-            return True
-    return False
 
-def get_valid_numbers():
-    valid_numbers = []
-    for k in tokens:
-        if one_num(k):
-            valid_numbers.append(k)
-    return valid_numbers
+        # print()
+        # exit()
+        if newly_generated in function_names:
+            return(newly_generated)
 
-print(get_valid_numbers())
+def get_params(function_name):
+    for fun in functions:
+        if fun['name'] == function_name:
+            return (list(fun['parameters'].keys()))
 
-"""
+def extract_next_number(prompt):
+    new = f"""
+    <im_start|>system
+    you are a tool to extract number parameters, for example "prompt": "What is the sum of 2 and 3?",
+    "parameters": {{"a": 2.0, "b": 3.0}}
+    <im_end|>
+    <im_start|>user
+    you will extract these numbers based on this prompt: {prompt}
+    <im_end|>
+    <im_start|>assistant
+    {prompt}
+    """
+    length_of_original = len(prompt)
+    start_of_answer = 0
+    for i in range(20, len(new)):
+        if new[i - 9: i] == 'assistant':
+            start_of_answer = i
+            break
+    print(start_of_answer)
+    exit()
+    while True:
+        pass
 
-p src/__main__.py
-Loading weights: 100%|████████████████████████████████████████████████████████████████████████████████████████████████████| 311/311 [00:00<00:00, 558.63it/s]
-['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '²', '³', '¹']
 
-"""
+
+def build_answer(user_request):
+    selected_function = get_function_names(user_request)
+    function_parameters = get_params(selected_function)
+    answer =  f'"prompt": "{user_request}",\n"name": "{selected_function}","'
+    
+
+extract_next_number(('"prompt": "What is the sum of 2 and 3?", "name": "fn_add_numbers", "parameters": {"a"'))
+
+print(build_answer("Say hi to the simo"))
+
+
